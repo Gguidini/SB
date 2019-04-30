e6f295289ca352875c3db4da8b372d9342a39e37
verasthiago <thiago.veras.machado@gmail.com>
2019-04-30 21:36:40 +0000

    Filter Line DONE (Better version)

--- assembler/src/pre_processor.cpp
+++ assembler/src/pre_processor.cpp
@@ -30,13 +30,14 @@ public:
     // Getters
     std::vector<std::string> get_buffer() { return __buffer;}
     std::string get_input_name() {return __input_name;}
-    std::string get_output_name() {return __output_name;}    
+    std::string get_output_name() {return __output_name;}
     std::ifstream& get_file_pointer(){return __file_pointer;}
     bool get_done(){return __done;}
     // Run
     std::vector<std::string> run();
     // FIXME: Remove this implementation an code correct one outside class declaration
     std::string generate_output() {return "SB2019-1";}
+    std::string filter_line(std::string&line);    
 };
 
 Pre_processor::Pre_processor(){
@@ -54,46 +55,52 @@ Pre_processor::Pre_processor(std::string input_name){
     __file_pointer.open(input_name);
 }
 
+std::string Pre_processor::filter_line(std::string &line){
+    std::string final_line = "";
+    for(int i = 0; i < (int)line.size(); i++){
+        // current char
+        char c = line[i];
+        // Erase comments
+        if(c == ';'){
+            break;
+        }
+        // Erase mutiples spaces and ending space
+        else if(c == ' '){
+            // Jump all spaces until end or next valid char
+            while(i < (int)line.size() and line[i] == ' ')i++;
+            // If has a valid char that is diff from comment, add space                
+            if(i < (int)line.size()){   
+                if(line[i] == ';') break;
+                final_line += " ";
+            }
+            // Get current char diff from space
+            i--;
+        }
+        // Add normal char
+        else if( c >= '!' and c <= '~' ){
+            final_line += c;
+        }
+    }
+    return final_line;
+}
+
 std::vector<std::string> Pre_processor::run(){
-    // TODO: Finish all steps
-    std::vector<std::string> aux;
+    
+    std::vector<std::string> processed_file;
     std::string line;
 
     while(getline(__file_pointer,line)){
-        char x;
-        bool valid_line = false;
-        std::string final_line = "";
-        for(int i = 0; i < (int)line.size(); i++){
-            // current char
-            char c = line[i];
-            // Erase comments
-            if(c == ';'){
-                break;
-            }
-            // Erase mutiples spaces and ending space
-            else if(c == ' '){
-                // Jump all spaces until end or next valid char
-                while(i < (int)line.size() and line[i] == ' ')i++;
-                // If has a valid char that is diff from comment, add space                
-                if(i < (int)line.size()){   
-                    if(line[i] == ';') break;
-                    final_line += " ";
-                }
-                // Get current char diff from space
-                i--;
-            }
-            // Add normal char
-            else if( c >= '!' and c <= '~' ){
-                final_line += c;
-                valid_line = true;
-            }
-        }
-        if(valid_line){
-            aux.push_back(final_line);
-        }
+
+        std::string processed_line = filter_line(line);
+        
+        // TODO : EXPAND MACROS
+        // TODO : SOLVE IFS
+        // TODO : SOLVE EQU
+
+        if(processed_line != "") processed_file.push_back(processed_line);
     }
     __done = true;
-    return aux;
+    return processed_file;
 }
 
 #endif
