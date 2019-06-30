8c2a8341d98c3d2d3b9429fea942c220ef3b6e24
Gguidini <giovanni.guidini@gmail.com>
2019-06-30 17:08:38 -0300

    Adicionando mais testes

---
+++ tradutor/test/sys_test/charsAndStrings.asm
@@ -0,0 +1,12 @@
+section text
+s_input string, 5
+c_output string
+c_output string + 1
+c_output string + 2
+c_output string + 3
+c_output string + 4
+s_output string, 5
+stop
+
+section data
+string: space 5
