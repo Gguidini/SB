0be14b63e7a27851cd4dd09a72f1a1b1d0439acb
verasthiago <thiago.veras.machado@gmail.com>
2019-07-02 03:32:07 +0000

    fixing some problems

--- ligador_ia32/src/linker.cpp
+++ ligador_ia32/src/linker.cpp
@@ -1,74 +1,96 @@
-// The pre processor classes.
-#ifndef PROCESSOR_H
-#define PROCESSOR_H
-// Dependencies
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <map>
-// Local dependencies
-#include "../lib/opcodes.h"
-#include "../lib/utils.h"
-#include "errors.cpp"
-#include "symbols.cpp"
+#include <../lib/elfio/elfio.hpp>
 
-// defines token symbols
-#define LABEL 0
-#define MACRO 1
-#define EQU 2
-#define IF 3
-#define ENDMACRO 5
-#define OP 4
-#define DIR 6     // SPACE or CONST
-#define ENDL 11
+class Linker{
 
-typedef std::pair<std::string, int> Token;
+  std::pair < char* , char* > __binary_code;
+  bool __done;
 
-// Processor class.
-// Processes a stream of tokens, transforming them into IA-32 binary code
-class Processor {
-    // Private attributes
-    // General information
-    std::string __input_name;
-    bool __done;
-    
-public:
+  public:
+
     // Constructors 
-    Processor();
-    Processor(std::string input);
-    // Process
-    std::vector<std::pair < std::vector<char>, std::vector<char> > > run();
+    Linker();
+    Linker(std::pair < char* , char* > binary_code);
 
-};
+    // Run
+    bool run();
+}
 
 
-// Processor sem arquivo de referência.
 // Nada é inicializado como válido.
-Processor::Processor(){
+Linker::Linker(){
     __done = false;
 }
 
-// Processor com arquivo de referência.
+// Linker com arquivo de referência.
 // Arquivo de referência já é aberto.
-Processor::Processor(std::string input_name){
-    __input_name = input_name;
+Linker::Linker(std::pair < char* , char* > binary_code){
+    __binary_code = binary_code;
     __done = false;
 }
 
-std::vector<std::pair < std::vector<char>, std::vector<char> > > Processor::Processor run(){
+bool Linker::Linker run(){
+
+    elfio ELFIO::writer;
     
-    fstream file(__input_name);
-    std::vector<std::pair < std::vector<char>, std::vector<char> > > processed;
+    // You can't proceed without this function call!
+    ELFIO::writer.create( ELFCLASS32, ELFDATA2LSB );
 
-    if(!file){
-        cout << "Invalid file name!" << endl;
-        return processed;
-    }
+    ELFIO::writer.set_os_abi( ELFOSABI_LINUX );
+    ELFIO::writer.set_type( ET_EXEC );
+    ELFIO::writer.set_machine( EM_386 );
 
-    // TODO : Finish this function
+    // Create code section
+    section* text_sec = ELFIO::writer.sections.add( ".text" );
+    text_sec->set_type( SHT_PROGBITS );
+    text_sec->set_flags( SHF_ALLOC | SHF_EXECINSTR );
+    text_sec->set_addr_align( 0x10 );
+    
 
+    text_sec->set_data( __binary_code.first , sizeof( __binary_code.first  ) );
+
+    // Create a loadable segment
+    segment* text_seg = ELFIO::writer.segments.add();
+    text_seg->set_type( PT_LOAD );
+    text_seg->set_virtual_address( 0x08048000 );
+    text_seg->set_physical_address( 0x08048000 );
+    text_seg->set_flags( PF_X | PF_R );
+    text_seg->set_align( 0x1000 );
+    
+    // Add code section into program segment
+    text_seg->add_section_index( text_sec->get_index(), text_sec->get_addr_align() );
+
+    // Create data section*
+    section* data_sec = ELFIO::writer.sections.add( ".data" );
+    data_sec->set_type( SHT_PROGBITS );
+    data_sec->set_flags( SHF_ALLOC | SHF_WRITE );
+    data_sec->set_addr_align( 0x4 );
+
+    data_sec->set_data( __binary_code.second, sizeof( __binary_code.second ) );
+
+    // Create a read/write segment
+    segment* data_seg = ELFIO::writer.segments.add();
+    data_seg->set_type( PT_LOAD );
+    data_seg->set_virtual_address( 0x08048020 );
+    data_seg->set_physical_address( 0x08048020 );
+    data_seg->set_flags( PF_W | PF_R );
+    data_seg->set_align( 0x10 );
+
+    // Add code section into program segment
+    data_seg->add_section_index( data_sec->get_index(), data_sec->get_addr_align() );
+
+    section* note_sec = ELFIO::writer.sections.add( ".note" );
+    note_sec->set_type( SHT_NOTE );
+    note_sec->set_addr_align( 1 );
+    note_section_accessor note_ELFIO::writer( ELFIO::writer, note_sec );
+    note_ELFIO::writer.add_note( 0x01, "Created by ELFIO", 0, 0 );
+    char descr[6] = {0x31, 0x32, 0x33, 0x34, 0x35, 0x36};
+    note_ELFIO::writer.add_note( 0x01, "Never easier!", descr, sizeof( descr ) );
+
+    // Setup entry point
+    ELFIO::writer.set_entry( 0x08048000 );
+
+    // Create ELF file
+    ELFIO::writer.save( "sb_ia32" );
+
+    return true;
 }
-
-
-
-#endif
